6d814b8d11fd9ece86d4a4cbbc8c01e3f323d374
parto <parto@DESKTOP-4PRT4SK>
2020-06-01 18:00:33 +0430

    رفع اشکال

--- ApiKarbord/App_Start/WebApiConfig.cs
+++ ApiKarbord/App_Start/WebApiConfig.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Web.Http;
 using System.Web.Http.Cors;
+using System.Web.Http.Routing;
 
 namespace ApiKarbord
 {
@@ -16,6 +18,10 @@ namespace ApiKarbord
             // Web API configuration and services
             var cors = new EnableCorsAttribute("*", "*", "*");
             config.EnableCors(cors);
+
+            var constraints = new { httpMethod = new HttpMethodConstraint(HttpMethod.Options) };
+            config.Routes.IgnoreRoute("OPTIONS", "*pathInfo", constraints);
+
             // Web API routes
             config.MapHttpAttributeRoutes();
 
