ab316920ff3f31a96e62d09070a2273d9c205b0d
parto <parto@DESKTOP-9B5K4FE>
2020-11-01 14:01:32 +0330

    رفع اشکال اتوماسیون

--- ApiKarbord/Controllers/AFI/data/Web_DataController.cs
+++ ApiKarbord/Controllers/AFI/data/Web_DataController.cs
@@ -914,13 +914,13 @@ namespace ApiKarbord.Controllers.AFI.data
         }
 
         // GET: api/Web_Data/Web_ErjUsers   ارجاع شونده/ارجاع دهنده
-        [Route("api/Web_Data/Web_ErjUsers/{ace}/{sal}/{group}")]
-        public async Task<IHttpActionResult> GetWeb_ErjUsers(string ace, string sal, string group)
+        [Route("api/Web_Data/Web_ErjUsers/{ace}/{sal}/{group}/{UserCode}")]
+        public async Task<IHttpActionResult> GetWeb_ErjUsers(string ace, string sal, string group, string userCode)
         {
             var dataAccount = UnitDatabase.ReadUserPassHeader(this.Request.Headers);
             if (UnitDatabase.CreateConection(dataAccount[0], dataAccount[1], ace, sal, group))
             {
-                string sql = string.Format(@"Select * from Web_ErjUsers");
+                string sql = string.Format(@"Select * from Web_ErjUsers('{0}')", userCode);
                 var listDB = UnitDatabase.db.Database.SqlQuery<Web_ErjUsers>(sql).ToList();
                 return Ok(listDB);
             }
