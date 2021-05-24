48fa8eb82adc9f21e7eb3a858c14bfadc4a7e6df
parto <parto@DESKTOP-9B5K4FE>
2021-05-24 16:50:56 +0430

    تغییر ورژن

--- ApiKarbord/Controllers/Unit/UnitPublic.cs
+++ ApiKarbord/Controllers/Unit/UnitPublic.cs
@@ -16,11 +16,11 @@ namespace ApiKarbord.Controllers.Unit
     {
 
         
-        public static int VerDB = 20;
+        public static int VerDB = 21;
         public static string conString = "";
 
         public static string titleVer = "API KarbordComputer Test : Ver ";
-        public static int VerNumber = 501;
+        public static int VerNumber = 502;
 
         //public static string titleVer = "API KarbordComputer : Ver ";
        // public static int VerNumber = 1015;
