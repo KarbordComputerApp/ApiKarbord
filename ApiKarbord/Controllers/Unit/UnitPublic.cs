71a2c624fc408a99f82a56d1b687d65bc519672b
parto <parto@DESKTOP-9B5K4FE>
2022-02-26 09:19:39 +0330

    رفع اشکال

--- ApiKarbord/Controllers/Unit/UnitPublic.cs
+++ ApiKarbord/Controllers/Unit/UnitPublic.cs
@@ -16,7 +16,7 @@ namespace ApiKarbord.Controllers.Unit
     {
 
 
-        public static int VerDB = 40;
+        public static int VerDB = 41;
         public static string conString = "";
 
         //public static string titleVer = "API KarbordComputer Test : Ver ";
