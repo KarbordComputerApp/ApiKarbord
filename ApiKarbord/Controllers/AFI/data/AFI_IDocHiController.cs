2efee2f2b7a65dee30af2c11475f35e993c485d5
KarbordComputerApp <62652601+KarbordComputerApp@users.noreply.github.com>
2022-05-07 17:52:28 +0430

    تست

--- ApiKarbord/Controllers/AFI/data/AFI_IDocHiController.cs
+++ ApiKarbord/Controllers/AFI/data/AFI_IDocHiController.cs
@@ -263,7 +263,7 @@ namespace ApiKarbord.Controllers.AFI.data
                 string[] serials = value.Split('-');
                 UnitDatabase.SaveLog(dataAccount[0], dataAccount[1], dataAccount[2], ace, sal, group, Convert.ToInt64(serials[0]), aFI_IDocHi.InOut == 1 ? "IIDoc" : "IODoc", 2, aFI_IDocHi.flagLog, 0);
 
-                return Ok(value);
+                return Ok(value); 
             }
             return Ok(con);
         }
