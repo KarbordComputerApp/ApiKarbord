1fb759795e67fc144333f10579c1a3c333791d0e
parto <parto@DESKTOP-9B5K4FE>
2022-09-12 11:14:49 +0430

    تغییر ورژن

--- ApiKarbord/Controllers/Unit/UnitPublic.cs
+++ ApiKarbord/Controllers/Unit/UnitPublic.cs
@@ -16,14 +16,14 @@ namespace ApiKarbord.Controllers.Unit
     {
 
 
-        public static int VerDB = 42;
+        public static int VerDB = 43;
         public static string conString = "";
 
         //public static string titleVer = "API KarbordComputer Test : Ver ";
         //public static int VerNumber = 516;
 
          public static string titleVer = "API KarbordComputer : Ver ";
-         public static int VerNumber = 1026;
+         public static int VerNumber = 1027;
 
 
         // Server.MapPath("ini/SqlServerConfig.Ini");
