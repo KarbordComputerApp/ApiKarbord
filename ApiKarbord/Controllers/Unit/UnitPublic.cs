f8868a4e1ccd1f10a9cb07f24b30a1787d7fb47c
karbord <karbord@SERVER>
2022-12-07 16:41:29 +0330

    ورژن قطعی 1028

--- ApiKarbord/Controllers/Unit/UnitPublic.cs
+++ ApiKarbord/Controllers/Unit/UnitPublic.cs
@@ -20,14 +20,14 @@ namespace ApiKarbord.Controllers.Unit
     {
 
 
-        public static int VerDB = 43;
+        public static int VerDB = 44;
         public static string conString = "";
 
         //public static string titleVer = "API KarbordComputer Test : Ver ";
         //public static int VerNumber = 516;
 
          public static string titleVer = "API KarbordComputer : Ver ";
-         public static int VerNumber = 1027;
+         public static int VerNumber = 1028;
 
 
         // Server.MapPath("ini/SqlServerConfig.Ini");
