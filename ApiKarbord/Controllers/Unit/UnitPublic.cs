8f427c49408dfe15ab45b3a5a04b4bb52f3e04d1
Admin <Admin@SERVER>
2024-09-23 11:39:51 +0330

    ورژن قطعی 1036

--- ApiKarbord/Controllers/Unit/UnitPublic.cs
+++ ApiKarbord/Controllers/Unit/UnitPublic.cs
@@ -19,14 +19,14 @@ namespace ApiKarbord.Controllers.Unit
     public class UnitPublic
     {
 
-        public static int VerDB = 51;
+        public static int VerDB = 52;
         public static string conString = "";
 
-        public static string titleVer = "API KarbordComputer Test : Ver ";
-        public static int VerNumber = 524;
+        //public static string titleVer = "API KarbordComputer Test : Ver ";
+        //public static int VerNumber = 524;
 
-       // public static string titleVer = "API KarbordComputer : Ver ";
-       // public static int VerNumber = 1035;
+        public static string titleVer = "API KarbordComputer : Ver ";
+        public static int VerNumber = 1036;
 
 
         // Server.MapPath("ini/SqlServerConfig.Ini");
